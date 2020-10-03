07651ff8903278fbac78730e75a1aaa57f159d58
yusuketanno <tylf1007@gmail.com>
2020-10-03 22:11:10 +0900

    コントローラー修正

---
+++ app/controllers/infos_controller.rb
@@ -0,0 +1,17 @@
+class InfosController < ApplicationController
+
+  def index
+    @infos = Info.all.order("created_at DESC")
+  end
+
+  def new
+    @info = Info.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+  end
+
+end
