6a24f82887de39f2ddd3fcafd40f8980c59cbb27
yusuketanno <tylf1007@gmail.com>
2020-10-04 09:51:49 +0900

    マイグレーション修正

--- db/migrate/20201002011328_create_infos.rb
+++ db/migrate/20201002011328_create_infos.rb
@@ -1,11 +1,11 @@
 class CreateInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :infos do |t|
-      t.string :fish_name  ,null: false
-      t.text :text         ,null: false
-      t.text :tackle       ,null: false
-      t.string :fish_num   ,null: false
-      t.string :image      ,null: false
+      t.string :fish_name  
+      t.text :text         
+      t.text :tackle       
+      t.string :fish_num   
+      t.string :image      
       t.timestamps
     end
   end
