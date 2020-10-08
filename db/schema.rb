5523ef4f92eb8c4f0f0c8709b588625bbed2ac70
yusuketanno <tylf1007@gmail.com>
2020-10-08 11:42:59 +0900

    マイグレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 2020_10_02_030637) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
